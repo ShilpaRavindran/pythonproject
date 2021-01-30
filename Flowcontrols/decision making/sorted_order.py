68b1f66f572524096db519e7d62a02c87c0820bd
ShilpaRavindran <shilparavindran123@gmail.com>
2021-01-30 20:27:59 +0530

    adding sorted order

--- Flowcontrols/decision making/sorted_order.py
+++ Flowcontrols/decision making/sorted_order.py
@@ -1,1 +1,22 @@
-num1=int(input())
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+num3=int(input("Enter num3"))
+if(num1>num2)&(num1>num3):
+    if(num2>num3):
+        print("sorted order is:",num1,num2,num3)
+    else:
+        print("sorted order is",num1,num3,num2)
+elif(num2>num1)&(num2>num3):
+    if(num3>num1):
+        print("sorted order is",num2,num3,num1)
+    else:
+        print("sorted order is",num2,num1,num3)
+elif(num3>num1)&(num3>num2):
+    if(num1>num2):
+        print("sorted order is",num3,num1,num2)
+    else:
+        print("sorted order is",num3,num2,num1)
+elif(num1==num2)&(num2==num3):
+    print("3 numbers are equal")
+else:
+    pass
